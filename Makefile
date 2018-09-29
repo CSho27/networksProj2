e7239de4be7d4da8104fd974985be295ec825044
CSho27 <shorte24@gmail.com>
2018-09-29 07:53:56 -0400

    Trying to resubmit this stuff. This now can place command line arguments into an array.

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
-all: proj2
+all: proj2.o
+	gcc -o proj2 proj2.o
 
-proj2: proj2.c
-	gcc -Wall -Werror -g -c proj2.c -o proj2
+proj2.o: proj2.c
+	gcc -Wall -Werror -g -c proj2.c
+	
+clean:
+	rm -f *.o
+	rm -f proj2
--- proj2.c
+++ proj2.c
@@ -1,8 +1,29 @@
 //Chris Shorter
 //This contains the main method for project 2
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE 80
 
 int main(){
-	printf("FUCK THIS SHIT");
+	char line[50];
+    char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
+	int run = 1;
+	
+    while(run == 1){
+        printf("web-client>");
+        fgets(line, sizeof(line), stdin);
+		char *token = strtok(line, " ");
+		int i=0;
+		while(token != NULL){
+			args[i] = token;
+			fflush(stdout);
+			if(strncmp(args[i], "-e", 2) == 0){
+				run = 0;
+			}
+			token = strtok(NULL, " ");
+			i++;
+		}
+	}
 	return 0;
 }
