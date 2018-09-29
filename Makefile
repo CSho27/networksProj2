b3d5fb07268cf4cc644fa498bf6d223a1abff8cd
CSho27 <shorte24@gmail.com>
2018-09-28 21:10:31 -0400

    First commit! woo woo!

---
+++ Makefile
@@ -0,0 +1,4 @@
+all: proj2
+
+proj2: proj2.c
+	gcc -Wall -Werror -g -c proj2.c -o proj2
---
+++ proj2.c
@@ -0,0 +1,8 @@
+//Chris Shorter
+//This contains the main method for project 2
+#include <stdio.h>
+
+int main(){
+	printf("FUCK THIS SHIT");
+	return 0;
+}
