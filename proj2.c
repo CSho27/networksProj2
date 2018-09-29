fc345dac9e04ffb41e064661c8cf56c8802373dc
CSho27 <shorte24@gmail.com>
2018-09-29 15:12:04 -0400

    Fixed it from being a shell program to handling flags

--- proj2.c
+++ proj2.c
@@ -2,28 +2,61 @@
 //This contains the main method for project 2
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_LINE 80
 
-int main(){
-	char line[50];
-    char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
-	int run = 1;
+int urlValid(char *url){
 	
-    while(run == 1){
-        printf("web-client>");
-        fgets(line, sizeof(line), stdin);
-		char *token = strtok(line, " ");
-		int i=0;
-		while(token != NULL){
-			args[i] = token;
-			fflush(stdout);
-			if(strncmp(args[i], "-e", 2) == 0){
-				run = 0;
+}
+
+int main(int argc, char *argv[]){
+	//booleans to record which flags are present
+	bool valid = true; 				//No invalid args, includes URL, etc.
+	bool url_present = false; 		//-u flag is there and a valid url follows it
+	bool print_details = false;		//-d is present, print the details
+	bool print_request = false;		//-r is present, so the program will print the get request
+	bool print_response = false;	//-R is present, so the program will print the response it gets
+	bool save_contents = false;		//-o is present, so the progrm will save the contents at the url to a file 
+	
+	//Strings for output file location and url
+	char *url = "";
+	char *contents_file = "";
+	
+	//check for flags/invalid arguments
+	for(int i=1; argv[i] != NULL; i++){
+		if(argv[i][0] == '-'){
+			switch(argv[i][1]){
+				case 'u':
+					url_present = true; 
+					i++;
+					url = argv[i];
+					//have a urlValid() method here or something
+					break;
+				case 'd':
+					print_details = true;
+					break;
+				case 'r':
+					print_request = true;
+					break;
+				case 'R':
+					print_response = true;
+					break;
+				case 'o':
+					save_contents = true;
+					i++;
+					contents_file = argv[i];
+					break;
+				default:
+					valid = false;
+					break;
 			}
-			token = strtok(NULL, " ");
-			i++;
+		}else{
+			valid = false;
 		}
 	}
+	
+	
+
 	return 0;
 }
